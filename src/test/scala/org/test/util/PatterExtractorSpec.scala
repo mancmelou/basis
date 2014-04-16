a6cf4ba147effa45f474f20cb0adc2729c17db5e
mancmelou <mancmelou@me.com>
2014-04-15 23:18:28 -0700

    Spec for the extractor

--- src/test/scala/org/test/util/PatterExtractorSpec.scala
+++ src/test/scala/org/test/util/PatterExtractorSpec.scala
@@ -18,7 +18,7 @@ class PatterExtractorSpec extends FunSpec with PatternExtractor {
 
     describe("extractRoutePattern") {
       it("returns regex pattern as a string") {
-        assert(extractRoutePattern("/pages/about")      == "^/pages/about/?$")
+        assert(extractRoutePattern("/pages/about")      == "/pages/about")
         assert(extractRoutePattern("/pages/:pageName")  == "^/pages/([A-z0-9]+)/?$")
         assert(extractRoutePattern("/:user/:page.html") == "^/([A-z0-9]+)/([A-z0-9]+).html/?$")
       }
