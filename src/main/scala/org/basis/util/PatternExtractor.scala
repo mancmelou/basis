38badb4d2422408789cedf5fac3d12b0a3c7554a
mancmelou <mancmelou@me.com>
2014-04-15 23:18:44 -0700

    Extractor code initial

--- src/main/scala/org/basis/util/PatternExtractor.scala
+++ src/main/scala/org/basis/util/PatternExtractor.scala
@@ -7,7 +7,7 @@ trait PatternExtractor {
 
   protected def extractRoutePattern(pattern: String): String = {
     if ("(?<=:)[A-z0-9_]+".r.findAllIn(pattern).isEmpty) {
-      "^" + "/+$".r.replaceAllIn(pattern, "") + "/?$"
+      "/+$".r.replaceAllIn(pattern, "")
     } else {
       "^" + ":[A-z0-9]+".r.replaceAllIn(pattern, "([A-z0-9]+)") + "/?$"
     }
