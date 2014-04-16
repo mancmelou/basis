396274f2745767a0cbfbecbb78f6ffd518f0dd40
mancmelou <mancmelou@me.com>
2014-04-15 23:19:45 -0700

    Spec for the router

--- src/test/scala/org/test/RouterSpec.scala
+++ src/test/scala/org/test/RouterSpec.scala
@@ -9,15 +9,15 @@ class RouterSpec extends FunSpec {
 
     describe("register") {
       it("registers a route with no params") {
-        router.register("get", "/", {})
+        router.register("get", "/", { })
 
-        assert(router.find("get", "/")._1 == Map.empty[String, String])
+        assert(router.find("get", "/").params == Map.empty[String, String])
       }
 
       it("registers a route with params") {
-        router.register("get", "/:user", {})
+        router.register("get", "/:user", { })
 
-        assert(router.find("get", "/JohnDoe")._1 == Map("user" -> "JohnDoe"))
+        assert(router.find("get", "/JohnDoe").params == Map("user" -> "JohnDoe"))
       }
     }
   }
