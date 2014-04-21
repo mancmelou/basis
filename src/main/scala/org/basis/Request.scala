c2db3aa858ad57e95bc567389ee92b62a8602693
mancmelou <mancmelou@me.com>
2014-04-21 08:43:03 -0700

    Added javadoc

--- src/main/scala/org/basis/Request.scala
+++ src/main/scala/org/basis/Request.scala
@@ -4,16 +4,29 @@ import scala.collection.mutable.Map
 import javax.servlet.http.HttpServletRequest
 import collection.JavaConversions.enumerationAsScalaIterator
 
-class Request(val req: HttpServletRequest, val routeParams: Map[String, String]) {
+class Request(private val req: HttpServletRequest, private val routeParams: Map[String, String]) {
+  /**
+   * Request headers container
+   */
   private val _headers = Map.empty[String, String]
+
+  /**
+   * Request GET and POST params container
+   */
   private val _params  = Map.empty[String, String]
 
+  /**
+   * Maps servlet request headers
+   */
   private def mapRequestHeaders() {
     for(name <- req.getHeaderNames()) {
       _headers(name.asInstanceOf[String]) = req.getHeader(name.asInstanceOf[String])
     }
   }
 
+  /**
+   * Maps servlet request params
+   */
   private def mapRequestParams() {
     for(name <- req.getParameterNames()) {
       _params(name.asInstanceOf[String]) = req.getParameter(name.asInstanceOf[String])
@@ -22,10 +35,34 @@ class Request(val req: HttpServletRequest, val routeParams: Map[String, String])
     _params ++= routeParams
   }
 
+  /**
+   * Returns the specified header's value
+   *
+   * @param name  Header name
+   * @return      Header value
+   */
   def header(name: String) = _headers.getOrElse(name, "")
 
+  /**
+   * Returns the specified param's value
+   *
+   * @param name  Param name
+   * @return      Param value
+   */
   def param(name: String)  = _params.getOrElse(name, "")
 
+  /**
+   * Returns current request's {@link HttpServletRequest} object
+   *
+   * @return
+   */
+  def servlet = req
+
+  /**
+   * Returns map of the fields stored in this object
+   *
+   * @return Mapping
+   */
   def toMap: Map[String, String] = {
     Map(
       "headers" -> _headers.toString,
@@ -33,6 +70,9 @@ class Request(val req: HttpServletRequest, val routeParams: Map[String, String])
     )
   }
 
+  /**
+   * Constructor procedure
+   */
   mapRequestHeaders()
   mapRequestParams()
 }
