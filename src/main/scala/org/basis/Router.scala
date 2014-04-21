39d03c826139f28fd1e98c39f08d7d6210336721
mancmelou <mancmelou@me.com>
2014-04-21 08:50:44 -0700

    javadoc for the Router class

--- src/main/scala/org/basis/Router.scala
+++ src/main/scala/org/basis/Router.scala
@@ -4,9 +4,22 @@ import scala.collection.mutable.Map
 import scala.util.matching.Regex
 import org.basis.util.PatternExtractor
 
+/**
+ * Matched route object that is returned after calling Router.find method
+ *
+ * @param params  Parameter map Map(name -> value)
+ * @param action  Block to execute for this route
+ */
 case class Route(params: Map[String, String], action: () => Any)
 
 class Router extends PatternExtractor {
+  /**
+   * Route pattern class
+   *
+   * @param url     Route URI pattern
+   * @param params  Route named params
+   * @param action  Route block to execute
+   */
   private case class Pattern(url: String, params: List[String], action: () => Any)
 
   /**
