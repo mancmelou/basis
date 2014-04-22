cd91898b970680e63375b5221b0514de392f3d2f
mancmelou <mancmelou@me.com>
2014-04-22 08:55:02 -0700

    Add more diversity to the example app

---
+++ src/main/scala/example/ExampleApp.scala
@@ -0,0 +1,43 @@
+package org.basis
+
+import java.util.Calendar
+
+class ExampleApp extends Basis {
+  get("/") {
+    header("X-App", "Basis")
+    header("Content-type", "text/html")
+
+    <h1>It works!</h1>
+    <p><a href="/debug">See debug info</a></p>
+    <p><a href="/form">Test form</a></p>
+    <small>Generated on: { Calendar.getInstance.getTime }</small>
+  }
+
+  get("/debug") {
+    header("Content-type", "text/plain")
+
+
+    "Router: "   + router.toString   + "\n\n" +
+    "Response: " + response.toString + " " + response.get.toMap + "\n\n" +
+    "Request: "  + request.toString  + " " + request.get.toMap + "\n\n"
+  }
+
+  get("/form") {
+    header("Content-type", "text/html")
+
+    html {
+      <form action="" method="post">
+        <input type="text" name="name" />
+        <input type="submit" value="Go!" />
+      </form>
+    }
+  }
+
+  post("/form") {
+    // async {
+    //  "Ping server" + "Some long calc"
+    // }
+
+    "HELLO " + param("name") + "!"
+  }
+}
