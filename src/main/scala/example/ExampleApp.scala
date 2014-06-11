a768d3baa9b24c8f743ef5e49a55f89d366ca880
mancmelou <mancmelou@me.com>
2014-06-11 08:16:05 -0700

    Simplify example app

--- src/main/scala/example/ExampleApp.scala
+++ src/main/scala/example/ExampleApp.scala
@@ -1,36 +1,16 @@
 package org.basis
 
 import java.util.Calendar
-import scala.concurrent._
-import ExecutionContext.Implicits.global
-import scala.sys.process._
-import java.io._
 
 class ExampleApp extends Basis {
   get("/") {
     header("X-App", "Basis")
     header("Content-type", "text/html")
 
-    future {
-      Thread.sleep(5000)
-
-      val pw = new PrintWriter(new File("/tmp/a" ))
-      pw.write(".")
-      pw.close
-    }
-
     <h1>It works!</h1>
     <p><a href="/debug">See debug info</a></p>
     <p><a href="/form">Test form</a></p>
     <small>Generated on: { Calendar.getInstance.getTime }</small>
-  }
-
-  get("/debug") {
-    header("Content-type", "text/plain")
-
-    "Router: "   + router.toString   + "\n\n" +
-    "Response: " + response.toString + " " + response.get.toMap + "\n\n" +
-    "Request: "  + request.toString  + " " + request.get.toMap + "\n\n"
   }
 
   get("/form") {
@@ -47,5 +27,13 @@ class ExampleApp extends Basis {
 
   post("/form") {
     "You submitted " + param("query") + "!"
+  }
+
+  get("/debug") {
+    header("Content-type", "text/plain")
+
+    "Router: "   + router.toString   + "\n\n" +
+      "Response: " + response.toString + " " + response.get.toMap + "\n\n" +
+      "Request: "  + request.toString  + " " + request.get.toMap + "\n\n"
   }
 }
