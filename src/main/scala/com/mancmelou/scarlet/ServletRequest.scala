1353dcd674499dfcf470340fed37f8802d817408
mancmelou <mancmelou@me.com>
2014-06-13 09:37:40 -0700

    Traits to interface HttpServletRequest and Response classes

--- src/main/scala/com/mancmelou/scarlet/ServletRequest.scala
+++ src/main/scala/com/mancmelou/scarlet/ServletRequest.scala
@@ -1,7 +1,9 @@
 package com.mancmelou.scarlet
 
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.{HttpSession, HttpServletRequest}
 
 trait ServletRequest extends HttpServletRequest {
-
+  def getMethod(): String
+  def getRequestURI(): String
+  def getSession(): HttpSession
 }
--- src/main/scala/com/mancmelou/scarlet/ServletResponse.scala
+++ src/main/scala/com/mancmelou/scarlet/ServletResponse.scala
@@ -1,13 +1,10 @@
 package com.mancmelou.scarlet
 
-import javax.servlet.http.{HttpSession, HttpServletResponse}
+import javax.servlet.http.HttpServletResponse
 import java.io.PrintWriter
 
 trait ServletResponse extends HttpServletResponse {
   def addHeader(name: String, value: String): Unit
   def setStatus(code: Int): Unit
   def getWritter(): PrintWriter
-  def getMethod(): String
-  def getRequestURI(): String
-  def getSession(): HttpSession
 }
