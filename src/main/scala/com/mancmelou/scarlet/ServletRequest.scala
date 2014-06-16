7de97b31a9e4cb5968f6c8d9a5fafc88f800bf56
mancmelou <mancmelou@me.com>
2014-06-16 08:50:29 -0700

    Removing interfaces I wont need

--- src/main/scala/com/mancmelou/scarlet/ServletRequest.scala
+++
@@ -1,9 +0,0 @@
-package com.mancmelou.scarlet
-
-import javax.servlet.http.{HttpSession, HttpServletRequest}
-
-trait ServletRequest extends HttpServletRequest {
-  def getMethod(): String
-  def getRequestURI(): String
-  def getSession(): HttpSession
-}
