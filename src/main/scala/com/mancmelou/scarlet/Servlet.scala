3de1426186568fb4ba0e6e1b07cbea2944d84fc5
mancmelou <mancmelou@me.com>
2014-06-19 08:07:51 -0700

    Removing methods that are not implemented in Application

--- src/main/scala/com/mancmelou/scarlet/Servlet.scala
+++ src/main/scala/com/mancmelou/scarlet/Servlet.scala
@@ -12,34 +12,18 @@ trait Servlet extends HttpServlet {
   def process(req: HttpServletRequest, res: HttpServletResponse): Unit
 
   /**
-   * Dispatches request from servlet
-   *
-   * @param req   A HttpServletRequest object
-   * @param res   A HttpServletResponse object
-   * @return      Return value from the action block
-   */
-  def dispatch(req: HttpServletRequest, res: HttpServletResponse): Any
-
-  /**
-   * Renders action result
-   *
-   * @param res
-   */
-  def render(res: HttpServletResponse): Unit
-
-  /**
    * Servlet GET request handler override
    *
-   * @param req
-   * @param res
+   * @param req HttpServletRequest
+   * @param res HttpServletResponse
    */
   override def doGet(req: HttpServletRequest, res: HttpServletResponse): Unit = process(req, res)
 
   /**
    * Servlet POST request handler override
    *
-   * @param req
-   * @param res
+   * @param req HttpServletRequest
+   * @param res HttpServletResponse
    */
   override def doPost(req: HttpServletRequest, res: HttpServletResponse): Unit = process(req, res)
 }
