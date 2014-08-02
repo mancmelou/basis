1aa18bbba5fdce1d1ba42f8cd459f473c50eb927
mancmelou <mancmelou@me.com>
2014-08-02 14:10:46 -0700

    Updating default app config

---
+++ src/main/scala/org/scarlet/util/DefaultApp.scala
@@ -0,0 +1,16 @@
+package org.scarlet.util
+
+import org.scarlet.ScarletApp
+
+/**
+ * Create a new class that extends the ScarletApp class.
+ */
+class DefaultApp extends ScarletApp {
+  /**
+   * This will register "GET /" route.
+   * Every GET request to the "/" URI will be routed to this block.
+   */
+  get("/") {
+    "Congratulations! You're running your first Scarlet app."
+  }
+}
